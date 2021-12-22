e1809efe3a67068f5875d130c71319fbf6970821
Vandana Kumari <vandanasingh31@gmail.com>
2021-12-22 09:00:01 -0600

    updated R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,2 +1,8 @@
-library(tidyverse)
-students <- read.csv("../R_Analysis/06_Evr_Tibble/Resources/students.csv")
+library(dplyr)
+vehicle_info <- read.csv("../MechaCar_Statistical_Analysis/MechaCar_mpg.csv")
+#pass in all six variables (i.e., columns)
+#add the dataframe you created in Step 4 as the data parameter
+predict_MPG<- lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance +AWD, data= vehicle_info)
+summary(predict_MPG)
+# Get the r-sqaured value. 
+summary(predict_MPG)$r.squared
